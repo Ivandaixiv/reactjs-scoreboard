9e4b70037a7aafb00df21ee90d3be1af3f221919
Ivan <ivandaixivwork@gmail.com>
2020-02-11 01:19:17 -0800

    Adds addition to score based on component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import Header from './Header'
-import Player from './Player'
+import Header from "./Header";
+import Player from "./Player";
 
 class App extends Component {
   state = {
@@ -9,51 +9,47 @@ class App extends Component {
       {
         name: "Guil",
         id: 1,
-        score: 0,
+        score: 0
       },
       {
         name: "Treasure",
         id: 2,
-        score: 0,
+        score: 0
       }
     ]
   };
 
   handleScoreChange = (index, delta) => {
-    console.log(index, delta)
-    // this.setState( prevState => ({
-    //   score: prevState.score + delta
-    // }));
-  }
+    this.setState(prevState => ({
+      score: (prevState.players[index].score += delta)
+    }));
+  };
 
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
       return {
         players: prevState.players.filter(p => p.id !== id)
       };
     });
-  }
+  };
 
   render() {
     return (
       <div className="scoreboard">
-        <Header 
-          title="Scoreboard" 
-          totalPlayers={this.state.players.length} 
-        />
-  
+        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+
         {/* Players list */}
-        {this.state.players.map( (player,index) =>
-          <Player 
+        {this.state.players.map((player, index) => (
+          <Player
             name={player.name}
             id={player.id}
-            key={player.id.toString()} 
+            key={player.id.toString()}
             index={index}
             removePlayer={this.handleRemovePlayer}
             score={player.score}
-            changeScore={this.handleScoreChange}          
+            changeScore={this.handleScoreChange}
           />
-        )}
+        ))}
       </div>
     );
   }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import Counter from './Counter'
+import Counter from "./Counter";
 
-const Player = (props) => {
+const Player = props => {
   return (
     <div className="player">
       <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
+        <button
+          className="remove-player"
+          onClick={() => props.removePlayer(props.id)}
+        >
+          ✖
+        </button>
+        {props.name}
       </span>
 
-      <Counter score={props.score} changeScore={props.changeScore} index={props.index}/>
+      <Counter
+        score={props.score}
+        changeScore={props.changeScore}
+        index={props.index}
+      />
     </div>
   );
-}
+};
 
 export default Player;
